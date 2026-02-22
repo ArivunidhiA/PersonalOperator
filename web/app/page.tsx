0dd9c7693872e85215c1f3b962ce38b2333e8512
ArivunidhiA <ariv.2612@gmail.com>
2026-02-22 17:42:19 -0500

    Fix Vercel build: mark home page as force-dynamic to prevent SSG prerender error with ClerkProvider

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,6 +1,8 @@
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import RealtimeVoice from "./components/RealtimeVoice";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <ErrorBoundary>
